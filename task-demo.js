7abdba48d8b1a163daa4bbdee1cb43d101dcee68
Matthew Latini <matt.latini@gmail.com>
2018-06-12 07:41:19 -0700

    even more static files..

--- task-demo.js
+++ task-demo.js
@@ -16,8 +16,7 @@ app.engine('.hbs', handlebars({
 }));
 app.set('view engine', '.hbs');
 
-app.use('/dist', express.static(__dirname + '/dist'));
-app.use('/public', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.use(cookieSession({
   name: 'session',
